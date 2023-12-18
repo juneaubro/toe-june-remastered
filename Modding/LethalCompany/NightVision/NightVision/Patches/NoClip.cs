cf0004dc5debc93b9c418747dc7ab699a26e36c1
Tony <toekneegaming123@gmail.com>
2023-12-18 00:42:30 -0500

    Update NoClip.cs

--- Modding/LethalCompany/NightVision/NightVision/Patches/NoClip.cs
+++ Modding/LethalCompany/NightVision/NightVision/Patches/NoClip.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
@@ -13,19 +8,27 @@ namespace NightVision.Patches
     [HarmonyPatch(typeof(PlayerControllerB))]
     internal class NoClip
     {
-        public static ModHotkey noclipKey = new ModHotkey(MouseAndKeyboard.PageDown, Thing);
-        private static PlayerControllerB controller;
+        public static ModHotkey noclipKey = new ModHotkey(MouseAndKeyboard.PageDown, NoClipToggle);
+        private static CharacterController controller = null;
+        private static PlayerControllerB playerController = null;
+        private static Rigidbody rigidbody = null;
         private static float fallValue = 0.0f;
         private static float fallValueUncapped = 0.0f;
+        private static float originalRadius = 0.0f;
         public static bool g_enabled = false;
+        public static bool setRadius = false;
         public static Vector3 originalGravity;
 
         [HarmonyPatch("Awake")]
         [HarmonyPostfix]
         public static void Awake(PlayerControllerB __instance)
         {
-            controller = __instance;
+            playerController = __instance;
             originalGravity = Physics.gravity;
+            controller = __instance.GetComponent<CharacterController>();
+            rigidbody = __instance.GetComponent<Rigidbody>();
+            setRadius = false;
+            originalRadius = controller.radius;
         }
 
         [HarmonyPatch("Update")]
@@ -35,27 +38,30 @@ namespace NightVision.Patches
             noclipKey.Update();
             if (g_enabled)
             {
-                __instance.fallValue = fallValue;
-                __instance.fallValueUncapped = fallValueUncapped;
-                Vector3 transformPosition = __instance.transform.position;
-                transformPosition.y = transformPosition.y;
+                __instance.fallValue = 0.0f;
+                __instance.fallValueUncapped = 0.0f;
+                __instance.takingFallDamage = false;
+                //Vector3 transformPosition = __instance.transform.position;
+                //transformPosition.y = transformPosition.y;
+                //__instance.transform.position = transformPosition;
             }
             else
             {
-                Physics.gravity = originalGravity;
+                if(controller != null)
+                    controller.radius = originalRadius;
             }
         }
 
 
-        public static void Thing()
+        public static void NoClipToggle()
         {
             g_enabled = !g_enabled;
 
-            Collider[] colliders = controller.GetComponents<Collider>();
+            Collider[] colliders = playerController.GetComponents<Collider>();
             if(colliders.Length == 0)
                 Debug.Log("no colliders -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
-            Collider[] ccolliders = controller.GetComponentsInChildren<Collider>();
+            Collider[] ccolliders = playerController.GetComponentsInChildren<Collider>();
             if(ccolliders.Length == 0)
                 Debug.Log("no child colliders -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 
@@ -67,10 +73,9 @@ namespace NightVision.Patches
                 {
                     fallValueUncapped = 0.0f;
                     fallValue = 0.0f;
-                    Debug.Log(col.name + " is false -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                    //Debug.Log(col.name + " is false -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
                 }
             }
-
             foreach (Collider col in ccolliders)
             {
                 col.enabled = !col.enabled;
@@ -78,9 +83,15 @@ namespace NightVision.Patches
                 {
                     fallValueUncapped = 0.0f;
                     fallValue = 0.0f;
-                    Debug.Log(col.name + " is false -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                    //Debug.Log(col.name + " is false -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
                 }
             }
+
+            controller.radius = Math.Abs(controller.radius - originalRadius) > 0.1 ? originalRadius : 0.0f;
+            Debug.Log($"controller radius: {controller.radius} -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Debug.Log($"setRadius: {setRadius} ==================================================================");
+            rigidbody.isKinematic = !rigidbody.isKinematic;
+            //setRadius = true;
         }
     }
 }
