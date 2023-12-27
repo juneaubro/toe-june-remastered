0da4aed50460c06ee2b172fab78a6af35af886a8
juneau <juneaugawat@hotmail.com>
2023-12-26 22:04:10 -0500

    Create XPGain.cs

---
+++ Modding/LethalCompany/NightVision/NightVision/Patches/XPGain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NightVision.Patches
+{
+    [HarmonyPatch(typeof(HUDManager))]
+    internal class XPGain
+    {
+        public static ModHotkey xpgain = new ModHotkey(MouseAndKeyboard.Numpad1, xptog);
+        static bool xptoggled = false;
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Update(HUDManager __instance)
+        {
+            xpgain.Update();
+            if(xptoggled)
+                __instance.localPlayerXP = 500;
+        }
+        public static void xptog()
+        {
+            xptoggled = !xptoggled;
+            Debug.Log($"xpgain is {xptoggled}");
+        }
+    }
+}
