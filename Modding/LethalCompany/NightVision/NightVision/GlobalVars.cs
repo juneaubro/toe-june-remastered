fbbc15ba69bf63c690548da0b7faf7fd17afd10b
juneau <juneaugawat@hotmail.com>
2024-01-02 23:30:17 -0500

    Create GlobalVars.cs

---
+++ Modding/LethalCompany/NightVision/NightVision/GlobalVars.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+
+namespace NightVision
+{
+    internal class GlobalVars
+    {
+    }
+
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundVars
+    {
+        public static PlayerControllerB lp;
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void Update()
+        {
+            if (GameNetworkManager.Instance.localPlayerController != null && lp == null)
+            {
+                lp = GameNetworkManager.Instance.localPlayerController;
+            }
+        }
+    }
+}
