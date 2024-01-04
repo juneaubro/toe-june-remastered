dcd2f93f1a3ffdb86832f220964df08ff11c62e8
juneau <juneaugawat@hotmail.com>
2024-01-04 00:41:39 -0500

    Create StartLever.cs
    Start the game without interacting with lever. Num9.

---
+++ Modding/LethalCompany/NightVision/NightVision/Patches/StartLever.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightVision.Patches
+{
+    [HarmonyPatch(typeof(StartMatchLever))]
+    internal class StartLever
+    {
+        public static ModHotkey startGameKey = new ModHotkey(MouseAndKeyboard.Numpad9, StartGame);
+        private static bool toggleStartGame = false;
+        [HarmonyPatch("Update")]
+        [HarmonyPrefix]
+        static void Update(StartMatchLever __instance)
+        {
+            startGameKey.Update();
+            if(toggleStartGame)
+                __instance.StartGame();
+        }
+        public static void StartGame()
+        {
+            toggleStartGame = !toggleStartGame;
+        }
+    }
+}
