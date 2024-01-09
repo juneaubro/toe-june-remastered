19fd8f054d07a24fb426e4f80402a29ba4504731
juneau <juneaugawat@hotmail.com>
2024-01-09 16:18:00 -0500

    Update Player.cs

--- Modding/LethalCompany/NightVision/NightVision/Patches/Player.cs
+++ Modding/LethalCompany/NightVision/NightVision/Patches/Player.cs
@@ -1,30 +1,30 @@
-﻿#nullable enable
-using GameNetcodeStuff;
-using HarmonyLib;
+﻿//#nullable enable
+//using GameNetcodeStuff;
+//using HarmonyLib;
 
-namespace NightVision.Patches
-{
+//namespace NightVision.Patches
+//{
 
-    [HarmonyPatch(typeof(PlayerControllerB))]
-    internal class Player
-    {
-        private static PlayerControllerB? _playerController;
+//    [HarmonyPatch(typeof(PlayerControllerB))]
+//    internal class Player
+//    {
+//        private static PlayerControllerB? _playerController;
 
-        [HarmonyPatch("Awake")]
-        [HarmonyPostfix]
-        public static PlayerControllerB? LocalPlayer()
-        {
-            return _playerController;
-        }
+//        [HarmonyPatch("Awake")]
+//        [HarmonyPostfix]
+//        public static PlayerControllerB? LocalPlayer()
+//        {
+//            return _playerController;
+//        }
 
-        [HarmonyPatch("Update")]
-        [HarmonyPostfix]
-        public static void Update()
-        {
-            if (GameNetworkManager.Instance == null)
-                return;
-            _playerController = GameNetworkManager.Instance.localPlayerController;
-        }
+//        [HarmonyPatch("Update")]
+//        [HarmonyPostfix]
+//        public static void Update()
+//        {
+//            if (GameNetworkManager.Instance == null)
+//                return;
+//            _playerController = GameNetworkManager.Instance.localPlayerController;
+//        }
 
-    }
-}
+//    }
+//}
