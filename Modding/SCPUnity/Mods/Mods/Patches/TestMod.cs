3c63481bb7a8b41f028a6a1bf1b8bd42bab5f9ab
juneau <juneaugawat@hotmail.com>
2024-06-12 12:16:18 -0400

    Modding for SCPUnity init

---
+++ Modding/SCPUnity/Mods/Mods/Patches/TestMod.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Mods.Patches
+{
+    [HarmonyPatch(typeof(Engine))]
+    internal class TestMod
+    {
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        static void Awake()
+        {
+            Debug.Log("Testing TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!\nTesting TestMod message!!!!!!");
+        }
+
+        // Well it worked and found out the engine script starts when the game starts and has a lot of shit.
+    }
+}
---
+++ Modding/SCPUnity/Mods/Mods/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+using Mods.Patches;
+
+namespace Mods
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ModsBase : BaseUnityPlugin
+    {
+        private const string modGUID = "JoesMod";
+        private const string modName = "JoesMod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModsBase Instance;
+
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("JoesMod started.\nJoesMod started.\nJoesMod started.\nJoesMod started.\nJoesMod started.\nJoesMod started.\nJoesMod started.\nJoesMod started.");
+
+            // Base
+            harmony.PatchAll(typeof(ModsBase));
+
+            //Debug
+            harmony.PatchAll(typeof(TestMod));
+        }
+    }
+}
