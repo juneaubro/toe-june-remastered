3881af0a1ce2f11f9b66e869c3a33afd968e47d4
Tony <toekneegaming123@gmail.com>
2024-06-19 18:11:53 -0400

    Fix scale for Multiplayer main menu button
    Also restructured other bits in MainMenuPatch

--- Modding/SCPUnity/Mods/Mods/Patches/MainMenuPatch.cs
+++ Modding/SCPUnity/Mods/Mods/Patches/MainMenuPatch.cs
@@ -32,13 +32,16 @@ namespace Mods.Patches
             }
         }
 
+        // Multiplayer button on Main Menu
         private static void CreateMultiplayerButton()
         {
             _buttonStack = _instance.mainMenuButtons.transform.GetChild(0).gameObject;
 
             GameObject copyObject = _buttonStack.transform.GetChild(1).gameObject;
             GameObject newObject = Object.Instantiate(copyObject);
+            newObject.name = "MultiplayerButton";
             newObject.transform.SetParent(_buttonStack.transform);
+            newObject.transform.localScale = copyObject.transform.localScale; // it does not automatically change this to scale after setting new parent
 
             Button button = newObject.transform.GetComponent<Button>();
             Button.ButtonClickedEvent newEvent = new Button.ButtonClickedEvent();
@@ -49,15 +52,13 @@ namespace Mods.Patches
             text.text = "MULTIPLAYER";
         }
 
+        // Listener method when Multiplayer button on Main Menu is clicked
         private static void OnMultiplayerClicked()
         {
             _instance.PlayClick();
+
             if (!_buttonCreated)
                 return;
-
-            // Need to call this to hide private _supportDevelopmentButton & _patreonSection
-            _instance.OnLoadGame();
-            _instance.loadGameScreen.SetActive(false);
 
             if(_multiplayerMenuObject == null)
                 CreateMultiplayerMenu();
@@ -80,6 +81,21 @@ namespace Mods.Patches
             _multiplayerMenuObject.transform.localPosition = Vector3.zero;
             _multiplayerMenuObject.SetActive(false);
 
+            // MULTIPLAYER BACK BUTTON
+            GameObject backButtonObject =
+                Object.Instantiate(_instance.mainMenuButtons.transform.GetChild(1).gameObject);
+            backButtonObject.transform.SetParent(_multiplayerMenuObject.transform);
+            // copied position is negative (????) so need to manually set its position
+            backButtonObject.transform.position = new Vector3(Screen.width * 0.1f, Screen.height * 0.2f);
+
+            Button backButton = backButtonObject.GetComponent<Button>();
+            Button.ButtonClickedEvent clickedEvent = new Button.ButtonClickedEvent();
+            clickedEvent.AddListener(OnBackClicked);
+            backButton.onClick = clickedEvent;
+
+            TextMeshProUGUI backText = backButtonObject.GetComponentInChildren<TextMeshProUGUI>();
+            backText.text = "BACK";
+
             // MULTIPLAYER MENU HEADER TEXT
             GameObject multiplayerMenuTextObject = new GameObject
             {
@@ -95,35 +111,30 @@ namespace Mods.Patches
             multiplayerText.text = "MULTIPLAYER";
             multiplayerText.color = Color.white;
             multiplayerText.fontSize = 54f;
+            multiplayerText.font = backText.font;
+            multiplayerText.fontMaterial = backText.fontMaterial;
             multiplayerText.alignment = TextAlignmentOptions.Center;
             multiplayerText.enableWordWrapping = false;
-
-            // MULTIPLAYER BACK BUTTON
-            GameObject backButtonObject =
-                Object.Instantiate(_instance.mainMenuButtons.transform.GetChild(1).gameObject);
-            backButtonObject.transform.SetParent(_multiplayerMenuObject.transform);
-            // copied position is negative (????) so need to manually set its position
-            backButtonObject.transform.position = new Vector3(Screen.width * 0.1f, Screen.height * 0.2f);
-
-            Button backButton = backButtonObject.GetComponent<Button>();
-            Button.ButtonClickedEvent clickedEvent = new Button.ButtonClickedEvent();
-            clickedEvent.AddListener(OnBackClicked);
-            backButton.onClick = clickedEvent;
-
-            TextMeshProUGUI backText = backButtonObject.GetComponentInChildren<TextMeshProUGUI>();
-            backText.text = "BACK";
         }
 
         private static void OnBackClicked()
         {
             ShowMultiplayerMenu(false);
-            _instance.mainMenuButtons.SetActive(true);
-            _instance.mainMenuLogo.SetActive(true);
         }
 
         public static void ShowMultiplayerMenu(bool state)
         {
             _multiplayerMenuObject.SetActive(state);
+
+            _instance.mainMenuButtons.SetActive(!state);
+            _instance.mainMenuLogo.SetActive(!state);
+            // need to find a way to hide/show private fields (_supportDevelopmentButton & _patreonSection)
+            if (state)
+            {
+                // need to call this to hide the remaining private fields
+                _instance.OnLoadGame();
+                _instance.loadGameScreen.SetActive(false);
+            }
         }
     }
 }
