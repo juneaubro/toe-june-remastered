03a8b013733a61ef1be3ae8516bce271c4167cee
Tony <toekneegaming123@gmail.com>
2024-06-19 15:53:07 -0400

    Back button in Multiplayer menu

--- Modding/SCPUnity/Mods/Mods/Patches/MainMenuPatch.cs
+++ Modding/SCPUnity/Mods/Mods/Patches/MainMenuPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using TheraBytes.BetterUi;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,7 +9,8 @@ namespace Mods.Patches
     [HarmonyPatch(typeof(MainMenu))]
     internal class MainMenuPatch
     {
-        private static GameObject buttonStack = null;
+        private static GameObject _multiplayerMenuObject = null;
+        private static GameObject _buttonStack = null;
         private static bool _buttonCreated = false;
         private static MainMenu _instance = null;
 
@@ -26,33 +28,28 @@ namespace Mods.Patches
                 _buttonCreated = true;
 
                 CreateMultiplayerButton();
-                
-                //Helpers.PrintGameObjectInfo(buttonStack, true);
+                CreateMultiplayerMenu();
             }
         }
 
-        public static void CreateMultiplayerButton()
+        private static void CreateMultiplayerButton()
         {
-            buttonStack = _instance.mainMenuButtons.transform.GetChild(0).gameObject;
+            _buttonStack = _instance.mainMenuButtons.transform.GetChild(0).gameObject;
 
-            GameObject copyObject = buttonStack.transform.GetChild(1).gameObject;
+            GameObject copyObject = _buttonStack.transform.GetChild(1).gameObject;
             GameObject newObject = Object.Instantiate(copyObject);
+            newObject.transform.SetParent(_buttonStack.transform);
 
             Button button = newObject.transform.GetComponent<Button>();
             Button.ButtonClickedEvent newEvent = new Button.ButtonClickedEvent();
             newEvent.AddListener(OnMultiplayerClicked);
             button.onClick = newEvent;
 
-            TextMeshProUGUI text = newObject.transform.GetComponentInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI text = button.transform.GetComponentInChildren<TextMeshProUGUI>();
             text.text = "MULTIPLAYER";
-
-            newObject.transform.SetParent(buttonStack.transform);
-            button.transform.SetParent(newObject.transform);
-            text.transform.SetParent(button.transform);
-            newObject.transform.position += new Vector3(-100, 0, 0);
         }
 
-        public static void OnMultiplayerClicked()
+        private static void OnMultiplayerClicked()
         {
             _instance.PlayClick();
             if (!_buttonCreated)
@@ -62,13 +59,71 @@ namespace Mods.Patches
             _instance.OnLoadGame();
             _instance.loadGameScreen.SetActive(false);
 
-            ShowMultiplayerMenu();
-            //Helpers.PrintGameObjectInfo(instance.gameObject, true);
+            if(_multiplayerMenuObject == null)
+                CreateMultiplayerMenu();
+
+            ShowMultiplayerMenu(true);
         }
 
-        public static void ShowMultiplayerMenu()
+        private static void CreateMultiplayerMenu()
         {
+            if (_multiplayerMenuObject != null)
+                return;
 
+            // MULTIPLAYER MENU OBJECT
+            _multiplayerMenuObject = new GameObject
+            {
+                name = "MultiplayerMenuObject"
+            };
+            _multiplayerMenuObject.transform.SetParent(_instance.transform);
+            _multiplayerMenuObject.transform.position = Vector3.zero;
+            _multiplayerMenuObject.transform.localPosition = Vector3.zero;
+            _multiplayerMenuObject.SetActive(false);
+
+            // MULTIPLAYER MENU HEADER TEXT
+            GameObject multiplayerMenuTextObject = new GameObject
+            {
+                name = "MultiplayerMenuText",
+                transform =
+                {
+                    localPosition = new Vector3(Screen.width * 0.5f, Screen.height * 0.9f, 0)
+                }
+            };
+            multiplayerMenuTextObject.transform.SetParent(_multiplayerMenuObject.transform);
+
+            TextMeshProUGUI multiplayerText = multiplayerMenuTextObject.AddComponent<TextMeshProUGUI>();
+            multiplayerText.text = "MULTIPLAYER";
+            multiplayerText.color = Color.white;
+            multiplayerText.fontSize = 54f;
+            multiplayerText.alignment = TextAlignmentOptions.Center;
+            multiplayerText.enableWordWrapping = false;
+
+            // MULTIPLAYER BACK BUTTON
+            GameObject backButtonObject =
+                Object.Instantiate(_instance.mainMenuButtons.transform.GetChild(1).gameObject);
+            backButtonObject.transform.SetParent(_multiplayerMenuObject.transform);
+            // copied position is negative (????) so need to manually set its position
+            backButtonObject.transform.position = new Vector3(Screen.width * 0.1f, Screen.height * 0.2f);
+
+            Button backButton = backButtonObject.GetComponent<Button>();
+            Button.ButtonClickedEvent clickedEvent = new Button.ButtonClickedEvent();
+            clickedEvent.AddListener(OnBackClicked);
+            backButton.onClick = clickedEvent;
+
+            TextMeshProUGUI backText = backButtonObject.GetComponentInChildren<TextMeshProUGUI>();
+            backText.text = "BACK";
+        }
+
+        private static void OnBackClicked()
+        {
+            ShowMultiplayerMenu(false);
+            _instance.mainMenuButtons.SetActive(true);
+            _instance.mainMenuLogo.SetActive(true);
+        }
+
+        public static void ShowMultiplayerMenu(bool state)
+        {
+            _multiplayerMenuObject.SetActive(state);
         }
     }
 }
