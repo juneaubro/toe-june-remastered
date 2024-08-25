0781de3320b08400de9467ab1674ebf63c9c45b1
juneau <juneaugawat@hotmail.com>
2024-08-25 14:47:03 -0400

    Risk Of Rain 2 Modding Environment
    Acrid Started It...

---
+++ Modding/RiskOfRain2/RandomMods/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+
+namespace RandomMods
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "AcridStartedIt";
+        private const string modName = "AcridStartedIt";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static Plugin Instance;
+
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"{modGUID} started loading.");
+
+            // Base
+            harmony.PatchAll(typeof(Plugin));
+
+
+        }
+    }
+}
+
