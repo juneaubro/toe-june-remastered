91c6ec321902e9dff49ece7931226b63a2a7b36b
juneau <juneaugawat@hotmail.com>
2024-08-26 18:13:13 -0400

    Poison and host updates.
    Poison damage change added to the dot initialization.
    Trying to bypass not being host. WIP.

--- Modding/RiskOfRain2/RandomMods/Patches/AcridPoison.cs
+++ Modding/RiskOfRain2/RandomMods/Patches/AcridPoison.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
 using RoR2;
+using UnityEngine;
 using static RoR2.DotController;
 
 namespace RandomMods.Patches
@@ -12,12 +13,38 @@ namespace RandomMods.Patches
     [HarmonyPatch(typeof(DotController))]
     internal class AcridPoison
     {
+        static float cd = 0f;
+        static float dmg = 1f;
+
+        [HarmonyPrefix]
+        [HarmonyPatch("Awake")]
+        static void Awake(ref DotController.DotDef[] ___dotDefs)
+        {
+            ___dotDefs[4].damageCoefficient = dmg;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("InitDotCatalog")]
+        static void InitDotCatalog(ref DotController.DotDef[] ___dotDefs)
+        {
+            ___dotDefs[4].damageCoefficient = dmg;
+        }
+
         [HarmonyPostfix]
         [HarmonyPatch("FixedUpdate")]
         static void FixedUpdate(DotController __instance, ref DotController.DotDef[] ___dotDefs)//, ref DotController.DotStackPool ___dotStackPool)
         {
-            ___dotDefs[4].damageCoefficient = 20.0f;
+            ___dotDefs[4].damageCoefficient = dmg;
 
+            //if (cd > 10)
+            //{
+            //    Debug.Log(ForceHost.hostStatus);
+            //    cd = 0;
+            //}
+            //else
+            //{
+            //    cd += UnityEngine.Time.deltaTime;
+            //}
             // DotController.DotStack dotStack1 = DotController.dotStackPool.Request();
         }
     }
---
+++ Modding/RiskOfRain2/RandomMods/Patches/ForceHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RoR2;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+namespace RandomMods.Patches
+{
+    [HarmonyPatch(typeof(NetworkServer))]
+    internal class ForceHost
+    {
+        public static bool hostStatus = false;
+
+        [HarmonyPrefix]
+        [HarmonyPatch("Update")]
+        static void Update(ref bool ___s_Active)
+        {
+            ___s_Active = true;
+            hostStatus = ___s_Active;
+        }
+    }
+}
