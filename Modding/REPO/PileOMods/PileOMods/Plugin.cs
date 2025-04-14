132bbcdfd7a2ad98eefa121c2977a47657c0442b
juneau <juneaugawat@hotmail.com>
2025-04-14 02:57:30 -0400

    added repo modding env

---
+++ Modding/REPO/PileOMods/PileOMods/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace PileOMods
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class PileOModsBase : BaseUnityPlugin
+    {
+        private const string modGUID = "PileOMods";
+        private const string modName = "PileOMods";
+        private const string modVersion = "1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static PileOModsBase Instance;
+
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("PileOMods started.");
+
+            // Base
+            harmony.PatchAll(typeof(PileOModsBase));
+        }
+    }
+}
---
+++ Modding/UnityModdingReferences/README.txt
@@ -0,0 +1,5 @@
+The dlls may need to be updated.
+
+These are the usual references needed.
+
+Better to use the references that are in the game.
